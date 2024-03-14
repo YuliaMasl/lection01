8caff44b393c531457de3bb90a2162dfaff226b0
Maslova Yuliya <tlstngv@gmail.com>
2024-03-14 21:14:51 +0800

    сумма рандомных чисел

---
+++ example006/Program.cs
@@ -0,0 +1,5 @@
+﻿int numderA = new Random().Next(1,10); //случайное число 1,2,3..8,9
+Console.WriteLine(numderA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numderA+numberB);
