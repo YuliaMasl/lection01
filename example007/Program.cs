579d432fa1326292e44261cd0697720fa2d7e5b1
Maslova Yuliya <tlstngv@gmail.com>
2024-03-14 21:55:46 +0800

    условие if else

---
+++ example007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Masha")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+Console.WriteLine(username);
+}
